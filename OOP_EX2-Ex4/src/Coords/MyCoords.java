7e146e47800812ee3c4d9b60f537e4cde6089b72
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-11-25 15:25:37 +0200

    Add files via upload

--- OOP_EX2-Ex4/src/Coords/MyCoords.java
+++ OOP_EX2-Ex4/src/Coords/MyCoords.java
@@ -5,7 +5,12 @@ import Geom.Point3D;
 public class MyCoords implements coords_converter{
 
 	public static void main(String[] args) {
-
+		MyCoords mc = new MyCoords();
+		Point3D p0 = new Point3D(32, 45);
+		Point3D p1 = new Point3D(33, 46);
+		System.out.println("Azimuth = " + mc.azimuth_elevation_dist(p0, p1)[0]);
+		System.out.println("Elevation = " + mc.azimuth_elevation_dist(p0, p1)[1]);
+		System.out.println("Distance = " + mc.azimuth_elevation_dist(p0, p1)[2]);
 	}
 
 	@Override
@@ -16,15 +21,13 @@ public class MyCoords implements coords_converter{
 	}
 
 	@Override
-	/**
-	 * We used the equations from this webstite:
-	 * https://www.omnicalculator.com/other/azimuth#how-to-calculate-the-azimuth-from-latitude-and-longitude
-	 */
 	public double distance3d(Point3D gps0, Point3D gps1) {
-		int radius = 6371;
-		double dx = gps1.x() - gps0.x();
-		double dy = gps1.y() - gps0.y();
-		double a = Math.pow(Math.sin(dx/2), 2) + Math.cos(gps0.x()) * Math.cos(gps1.x()) * Math.pow(Math.sin(dy/2), 2);
+		int radius = 6371000;
+		double lat0 = Math.toRadians(gps0.x()); double lon0 = Math.toRadians(gps0.y());
+		double lat1 = Math.toRadians(gps1.x()); double lon1 = Math.toRadians(gps1.y());
+		double dlat = Math.toRadians(gps1.x() - gps0.x());
+		double dlon = Math.toRadians(gps1.y() - gps0.y());
+		double a = Math.pow(Math.sin(dlat/2),2) + Math.cos(lat0) * Math.cos(lat1) * Math.pow(Math.sin(dlon/2),2);
 		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 		return radius * c;
 	}
@@ -38,15 +41,23 @@ public class MyCoords implements coords_converter{
 	}
 
 	@Override
+	/**
+	 * We used these websites:
+	 * https://www.omnicalculator.com/other/azimuth#how-to-calculate-the-azimuth-from-latitude-and-longitude
+	 * https://www.photopills.com/articles/understanding-azimuth-and-elevation
+	 */
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
 		double[] answer = new double[3];
-		double dx = gps1.x() - gps0.x();
-		double dy = gps1.y() - gps0.y();
-		double azimuth = Math.atan2(Math.sin(dy) * Math.cos(gps1.x()), Math.cos(gps0.x()) * Math.sin(gps1.x()) - Math.sin(gps0.x())
-						 * Math.cos(gps1.x()) * Math.cos(dy));
+		double lat0 = Math.toRadians(gps0.x()); double lon0 = Math.toRadians(gps0.y());
+		double lat1 = Math.toRadians(gps1.x()); double lon1 = Math.toRadians(gps1.y());
+		double dlat = Math.toRadians(gps1.x() - gps0.x());
+		double dlon = Math.toRadians(gps1.y() - gps0.y());
+		double azimuth = Math.atan2(Math.sin(dlon) * Math.cos(lat1), Math.cos(lat0) * Math.sin(lat1) - Math.sin(lat0)
+						 * Math.cos(lat1) * Math.cos(dlon));
 		double distance = distance3d(gps0, gps1);
-		answer[0] = azimuth;
-//		answer[1] = elevation;
+		double elevation = Math.asin(distance3d(new Point3D(lat1, lat0), gps1)/distance3d(gps0, gps1));
+		answer[0] = Math.toDegrees(azimuth);
+		answer[1] = elevation;
 		answer[2] = distance;
 		return answer;
 	}
