24518094b94a0eba8dd0bdff7be7fcfbdbd0564f
USER <USER@USER-PC>
2018-11-27 11:23:53 +0200

    test

--- OOP_EX2-Ex4/src/Geom/Point3D.java
+++ OOP_EX2-Ex4/src/Geom/Point3D.java
@@ -9,7 +9,7 @@ public class Point3D implements Geom_element, Serializable
 	private static final long serialVersionUID = 1L;
 	private double _x,_y,_z;
 
-	public Point3D(double x,double y,double z) 
+	public Point3D(double x,double y,double z)  
 	{
 		_x=x;
 		_y=y;
