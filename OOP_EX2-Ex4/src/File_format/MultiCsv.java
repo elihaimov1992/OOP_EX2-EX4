604f25d6abae8867feaa7c133adcf4dbd907e0db
elihaimov1992 <elimandbd69@gmail.com>
2018-12-02 15:51:45 +0200

    added test

--- OOP_EX2-Ex4/src/File_format/MultiCsv.java
+++ OOP_EX2-Ex4/src/File_format/MultiCsv.java
@@ -19,7 +19,8 @@ public class MultiCsv {
 	 * @return
 	 */
 	public static void csvFiles2kml(String folder_path) {
-		GIS_project
+		GIS_project_obj project = csvFiles2project(folder_path);
+		GisProject2kml(project, folder_path);
 	}
 	
 	public static GIS_project_obj csvFiles2project(String folder_path) {
@@ -34,14 +35,14 @@ public class MultiCsv {
 		
 		Csv2Gis c2g = new Csv2Gis();
 		for (int i = 0; i < csvFiles.length; i++) {
-			GIS_layer_obj layer = c2g.csv2GisLayer(csvFiles[i].getName());
+			GIS_layer_obj layer = c2g.csv2GisLayer(csvFiles[i].getAbsolutePath());
 			project.add(layer);
 		}
 		
 		return project;
 	}
 	
-	public static void GisProject2kml(GIS_project_obj project) {
+	public static void GisProject2kml(GIS_project_obj project, String folder_path) {
 		ArrayList<String> content = new ArrayList<String>();
 	    String kmlstart = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
 	            "<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Style id=\"red\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href></Icon></IconStyle></Style><Style id=\"yellow\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href></Icon></IconStyle></Style><Style id=\"green\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon></IconStyle></Style><Folder><name>Wifi Networks</name>\n";
@@ -50,27 +51,29 @@ public class MultiCsv {
         
         try{
         	int kml_name = 0;
-        	boolean check = new File("//", Integer.toString(kml_name)).exists();
+        	boolean check = new File(folder_path, Integer.toString(kml_name)).exists();
         	while (check) {
         		kml_name++;
-        		check = new File("//", Integer.toString(kml_name)).exists();
+        		check = new File(folder_path, Integer.toString(kml_name)).exists();
         	}
-	        FileWriter fw = new FileWriter("multiCsv" + kml_name + ".kml");
+	        FileWriter fw = new FileWriter(folder_path +"//multiCsv" + kml_name + ".kml");
 	        BufferedWriter bw = new BufferedWriter(fw);
 	        Iterator<GIS_layer> layer_it = project.iterator();
 	        while(layer_it.hasNext()) {
 	        	GIS_layer layer = layer_it.next();
 	        	Iterator<GIS_element> elem_it = layer.iterator();
-	        	GIS_element_obj elem = (GIS_element_obj) elem_it.next();
-	        	Meta_data_obj metadata = (Meta_data_obj)elem.getData();
-	            String kmlelement ="<Placemark>\n" +
-	                    "<name><![CDATA["+metadata.getName()+"]]></name>\n" +
-	                    "<description>"+"<![CDATA[BSSID: <b>"+metadata.getBssid()+"</b><br/>Capabilities: <b>"+metadata.getCapabilities()+"</b><br/>Timestamp: <b>"+metadata.getTimestamp()+"</b><br/>Date: <b>"+metadata.getDate()+"</b>]]>"+"</description><styleUrl>#red</styleUrl>\n" +
-	                    "<Point>\n" +
-	                    "<coordinates>"+metadata.getCurrentLatitude()+","+metadata.getCurrentLongitude()+"</coordinates>" +
-	                    "</Point>\n" +
-	                    "</Placemark>\n";
-	            content.add(kmlelement);
+	        	while(elem_it.hasNext()) {
+	        		GIS_element_obj elem = (GIS_element_obj) elem_it.next();
+		        	Meta_data_obj metadata = (Meta_data_obj)elem.getData();
+		            String kmlelement ="<Placemark>\n" +
+		                    "<name><![CDATA["+metadata.getName()+"]]></name>\n" +
+		                    "<description>"+"<![CDATA[BSSID: <b>"+metadata.getBssid()+"</b><br/>Capabilities: <b>"+metadata.getCapabilities()+"</b><br/>Timestamp: <b>"+metadata.getTimestamp()+"</b><br/>Date: <b>"+metadata.getDate()+"</b>]]>"+"</description><styleUrl>#red</styleUrl>\n" +
+		                    "<Point>\n" +
+		                    "<coordinates>"+metadata.getCurrentLatitude()+","+metadata.getCurrentLongitude()+"</coordinates>" +
+		                    "</Point>\n" +
+		                    "</Placemark>\n";
+		            content.add(kmlelement);
+	        	}
 	        }
 	        content.add(kmlend);
 	        String csv = content.toString().replaceAll(", ", "").replace("[<", "<").replace("l>]", "l>");
